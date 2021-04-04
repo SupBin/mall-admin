566e8b9733c7c2b75391e40c647f58b968b79c15
SupBin <1097512258@qq.com>
2021-04-04 08:13:47 +0800

    init layout

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
+import Home from '../views/layout/Home.vue';
+import Login from '../views/layout/Login.vue';
 
 Vue.use(VueRouter);
 
@@ -11,12 +12,13 @@ const routes = [
     component: Home,
   },
   {
+    path: '/login',
+    name: 'Login',
+    component: Login,
+  },
+  {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
   },
 ];
 
---
+++ src/views/layout/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="home">
+    <left-menu/>
+    <div :class="{ 'main-app': true, 'menu-unfold': $store.state.collapsed }">
+        <slider-list/>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import LeftMenu from '@/views/layout/components/menu.vue';
+import SliderList from '@/views/layout/components/sliderNav.vue';
+
+export default {
+  components: {
+    LeftMenu,
+    SliderList,
+
+  },
+  data() {
+    return {
+
+    };
+  },
+  methods: {
+    toggleCollapsed() {
+      this.collapsed = !this.collapsed;
+    },
+  },
+};
+</script>
+
+<style lang = "less">
+@import url("~@/assets/css/home.less");
+</style>
